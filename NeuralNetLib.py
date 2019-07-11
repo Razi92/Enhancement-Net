dc60904248b078bd7c507d4a45a3b6d7362c454a
Razi <razi.rezaei@tum.de>
2019-07-11 17:29:23 +0200

    Update NeuralNetLib.py

--- NeuralNetLib.py
+++ NeuralNetLib.py
@@ -1,1 +1,8 @@
 from __future__ import division
+import os
+import numpy as np
+import scipy.misc
+from math import floor
+frim PIL import image
+import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
